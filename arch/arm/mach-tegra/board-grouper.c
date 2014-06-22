b3695cff333faa50d9fea2f252c4690f15327a44
enssorcel <callumross12@gmail.com>
2014-06-22 20:16:23 +0100

    k3.3: grouper: Updates to board files
    Beginning to remove the structs etc. which caused errors, and update
    them according to Nvidia's commits to the kai board. This is similar
    enough to grouper that hopefully the vast majority of errors can be
    removed and at least get the kernel compiling with grouper support.

--- arch/arm/mach-tegra/board-grouper.c
+++ arch/arm/mach-tegra/board-grouper.c
@@ -851,11 +851,13 @@ static void __init tegra_grouper_reserve(void)
 }
 
 MACHINE_START(GROUPER, "grouper")
-	.boot_params	= 0x80000100,
+	.atag_offset	= 0x100
 	.map_io		= tegra_map_common_io,
 	.reserve	= tegra_grouper_reserve,
-	.init_early	= tegra_init_early,
+	.init_early	= tegra30_init_early,
 	.init_irq	= tegra_init_irq,
+	.handle_irq	= gic_handle_irq,
 	.timer		= &tegra_timer,
 	.init_machine	= tegra_grouper_init,
+	.restart	= tegra_assert_system_reset,
 MACHINE_END
